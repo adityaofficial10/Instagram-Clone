641daa1842a7500e20a66775de1e27035b2b1aaa
ADITYA R RUDRA <adityaberasia@gmail.com>
2021-01-04 21:20:49 +0530

    Add files via upload

---
+++ config/fbCred.js
@@ -0,0 +1,8 @@
+
+export const apiKey = "AIzaSyA7XF1ZFpYM6a_ozUN3eRFUXOhAFvMf_e0";
+export const authDomain = "instagramclone-887fe.firebaseapp.com";
+export const projectId = "instagramclone-887fe";
+export const storageBucket = "instagramclone-887fe.appspot.com";
+export const messagingSenderId = "966890532175";
+export const appId = "1:966890532175:web:9e3e724369693db596293a";
+export const measurementId = "G-YEJ2N97RWR";
